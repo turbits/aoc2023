d35ce76636888407275866898da8da630b010ab5
turbits <me@turbits.sh>
2024-05-18 19:57:08 -0700

    wip d3

---
+++ aoc2023/Day3/Day3Program.cs
@@ -0,0 +1,8 @@
+﻿namespace AOC2023.Day3; 
+
+public class Day3Program {
+    public static void RunPart1() {
+        using StreamReader sr = new(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Day2/input.txt"));
+        while (sr.ReadLine() is { } line) { }
+    }
+}
---
+++ aoc2023/Day3/SubMenu.cs
@@ -0,0 +1,5 @@
+﻿namespace AOC2023.Day3; 
+
+public class SubMenu {
+    
+}
